7b2fa80f6086f6d9c66560096cd5984090a04dc4
dong <116865376@qq.com>
2013-04-12 23:28:20 +0800

    add kernel source

---
+++ kernel_v1/include/linux/cramfs_fs_sb.h
@@ -0,0 +1,26 @@
+#ifndef _CRAMFS_FS_SB
+#define _CRAMFS_FS_SB
+
+/*
+ * cramfs super-block data in memory
+ */
+struct cramfs_sb_info {
+			unsigned long magic;
+			unsigned long size;
+			unsigned long blocks;
+			unsigned long files;
+			unsigned long flags;
+
+//jkeqiang
+			int nand;
+			int *block_map;
+			int nblock;
+			struct mtd_info *mtd;
+};
+
+static inline struct cramfs_sb_info *CRAMFS_SB(struct super_block *sb)
+{
+	return sb->s_fs_info;
+}
+
+#endif
